4eda3f9d4e8bae38472f80f3b54fc4dbaa42fb7a
souvikpal <souvik_pal1@persistent.com>
2022-02-27 20:49:11 +0530

    remove some unnecessary write andd branch name

--- math.py
+++ math.py
@@ -3,13 +3,13 @@ def add(x,y):
     return x+y
 #Substrack Implementation
 def substrack(x,y):
-    return x-y   #on master branch
+    return x-y   
 #Multiply Implementation
 def multiply(x,y):
-    return x*y   # on Bug456
+    return x*y          # on Bug456 Branch
 #Divide Implementation
 def divide(x,y):
-    if y==0 :           #on Bug789 branch
+    if y==0 :           
         return Divide_By_0_Error
     else:
         return x/y
